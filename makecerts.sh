638b0175ec9f54c32db17e71670633fc6f0c46c9
Marcelo Cantos <marcelo.cantos@gmail.com>
2019-09-29 13:24:48 +1000

    Auto-config certs

--- makecerts.sh
+++ makecerts.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+openssl req -config openssl.cnf -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
