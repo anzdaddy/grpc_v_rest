81080474c30d4d5b539a7554f7949413a6ba2157
Marcelo Cantos <marcelo.cantos@gmail.com>
2019-09-29 14:17:51 +1000

    Listening address flags

--- common.go
+++ common.go
@@ -12,17 +12,18 @@ type tlsCreds struct {
 }
 
 func main() {
+	grpcAddr := flag.String("grpc", ":4443", "gRPC listening address")
+	restAddr := flag.String("rest", ":4444", "ReST listening address")
 	var flagCreds tlsCreds
 	flag.StringVar(&flagCreds.certFile, "cert", "cert.pem", "TLS Cert File")
 	flag.StringVar(&flagCreds.keyFile, "key", "key.pem", "TLS Key File")
 	flag.Parse()
-	grpcAddr := ":4443"
-	mainGRPC(grpcAddr, flagCreds)
-	logrus.Infof("gRPC %s", grpcAddr)
 
-	restAddr := ":4444"
-	mainREST(restAddr, flagCreds)
-	logrus.Infof("REST %s", restAddr)
+	mainGRPC(*grpcAddr, flagCreds)
+	logrus.Infof("gRPC %s", *grpcAddr)
+
+	mainREST(*restAddr, flagCreds)
+	logrus.Infof("REST %s", *restAddr)
 
 	<-make(chan struct{})
 }
